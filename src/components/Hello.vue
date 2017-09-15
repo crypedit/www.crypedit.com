9f219d7dc1ca05cd516d9418a38851fa9903e34d
Fan Jiang <fanjiang@thoughtworks.com>
2017-09-15 14:46:17 +0800

    use vue-particles instead

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div id="hello">
+      <h1>{{ msg }}</h1>
+      <h2>{{ slogan }}</h2>
+    </div>
+    <vue-particles
+      color="#0e0e0e"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    >
+    </vue-particles>
   </div>
 </template>
 
@@ -9,8 +30,11 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'crypedit, credit, crypted'
+      msg: 'crypedit, credit, crypted',
+      slogan: 'make trust worthy'
     }
+  },
+  mounted () {
   }
 }
 </script>
@@ -33,5 +57,20 @@ li {
 
 a {
   color: #42b983;
+}
+
+#hello {
+  z-index: 999;
+  margin-top: 100px;
+  right: 3rem;
+}
+
+#particles-js {
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
