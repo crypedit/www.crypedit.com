b8ced401e47de2dd58c18216e44288298cdf609b
Fan Jiang <fanjiang@thoughtworks.com>
2017-09-18 02:22:00 +0800

    comment out unsupported uglifyjs job

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -31,12 +31,13 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
-    }),
+    //XXX: vue-particles is not supported, need to check here later
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   },
+    //   sourceMap: true
+    // }),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
